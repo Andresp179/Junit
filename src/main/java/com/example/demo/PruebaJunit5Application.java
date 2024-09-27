a8df5267f5652f8bc69ce4925421b3451db38828
Andres Perez <perezandres1799@gmail.com>
2024-09-27 18:16:58 -0500

    Proyect

---
+++ src/main/java/com/example/demo/PruebaJunit5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaJunit5Application.class, args);
+	}
+
+}
